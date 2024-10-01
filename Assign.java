b75cb47f178d3d50223847d71d8cd1e2416a3b75
j2501 <72064756+j2501@users.noreply.github.com>
2024-10-01 13:50:50 -0400

    Update Assign.java

--- Assign.java
+++ Assign.java
@@ -1,49 +1,19 @@
-    import java.util.Scanner;  
-    import java.lang.Math;  
-    public class ArmstsrongNumberExample  
-    {  
-    //function to check if the number is Armstrong or not  
-    static boolean isArmstrong(int n)   
-    {   
-    int temp, digits=0, last=0, sum=0;   
-    //assigning n into a temp variable  
-    temp=n;   
-    //loop execute until the condition becomes false  
-    while(temp>0)    
-    {   
-    temp = temp/10;   
-    digits++;   
+    public class PrimeExample{    
+     public static void main(String args[]){    
+      int i,m=0,flag=0;      
+      int n=3;//it is the number to be checked    
+      m=n/2;      
+      if(n==0||n==1){  
+       System.out.println(n+" is not prime number");      
+      }else{  
+       for(i=2;i<=m;i++){      
+        if(n%i==0){      
+         System.out.println(n+" is not prime number");      
+         flag=1;      
+         break;      
+        }      
+       }      
+       if(flag==0)  { System.out.println(n+" is prime number"); }  
+      }//end of else  
+    }    
     }   
-    temp = n;   
-    while(temp>0)   
-    {   
-    //determines the last digit from the number      
-    last = temp % 10;   
-    //calculates the power of a number up to digit times and add the resultant to the sum variable  
-    sum +=  (Math.pow(last, digits));   
-    //removes the last digit   
-    temp = temp/10;   
-    }  
-    //compares the sum with n  
-    if(n==sum)   
-    //returns if sum and n are equal  
-    return true;      
-    //returns false if sum and n are not equal  
-    else return false;   
-    }   
-    //driver code  
-    public static void main(String args[])     
-    {     
-    int num;   
-    Scanner sc= new Scanner(System.in);  
-    System.out.print("Enter the limit: ");  
-    //reads the limit from the user  
-    num=sc.nextInt();  
-    System.out.println("Armstrong Number up to "+ num + " are: ");  
-    for(int i=0; i<=num; i++)  
-    //function calling  
-    if(isArmstrong(i))  
-    //prints the armstrong numbers  
-    System.out.print(i+ ", ");  
-    }   
-    }
